bf5801707c9e57a39fbe0deab4cc61efef5780c1
Chris Martin <christopher.martin@flhealth.gov>
2025-02-20 11:00:54 -0600

    Doing more stuff

---
+++ EGON.DiscordBot/Models/ScheduledMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGON.DiscordBot.Models
+{
+    public class ScheduledMessage
+    {
+        public ulong UserId { get; set; }
+        public string EventId { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset SendTime { get; set; }
+    }
+}
