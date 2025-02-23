c4c7d99d99b47ed71f8d35c152315448fdf5c05c
Chris Martin <christopher.martin@flhealth.gov>
2025-02-23 13:28:36 -0600

    Fixed up tentative signups sending a reminder, and event cancellation

---
+++ EGON.Library/Models/Entities/EchelonEventEntity.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace EGON.Library.Models.Entities
+{
+    public class EchelonEventEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }  // E.g., "Raid", "Mythic", "Meeting"
+        public string RowKey { get; set; }  // Unique event ID
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTimeOffset EventDateTime { get; set; }
+        public string Organizer { get; set; }
+        public string ImageUrl { get; set; }
+        public string Footer { get; set; }
+        public ulong MessageId { get; set; }
+        public string MessageUrl { get; set; }
+
+        // This HAS to be a string for the query, a ulong fails to convert properly
+        public string EventId { get; set; }
+
+
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+
+}
---
+++ EGON.Library/Models/NewTeamRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EGON.Library.Models
+{
+    public class NewTeamRequest
+    {
+        public Guid Id { get; set; }
+        public InstanceType ForInstanceType { get; set; }
+    }
+}
