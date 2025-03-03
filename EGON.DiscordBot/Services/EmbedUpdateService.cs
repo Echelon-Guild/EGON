9eddf72e615b438b4beb07e926d5516c2bbffb2c
Chris Martin <christopher.martin@flhealth.gov>
2025-03-03 14:09:39 -0600

    Moved eventupdate to it's own service so I can stop copying it

---
+++ EGON.DiscordBot/Services/EmbedUpdateService.cs
@@ -0,0 +1,63 @@
+﻿using Discord;
+using Discord.WebSocket;
+using EGON.DiscordBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGON.DiscordBot.Services
+{
+    public class EmbedUpdateService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly StorageService _storageService;
+        private readonly EmbedFactory _embedFactory;
+
+        public EmbedUpdateService(DiscordSocketClient client, StorageService storageService, EmbedFactory embedFactory)
+        {
+            _client = client;
+            _storageService = storageService;
+            _embedFactory = embedFactory;
+        }
+
+        public async Task UpdateEventEmbed(ulong eventId, bool cancelled = false, bool closed = false)
+        {
+            // Retrieve event entity (including MessageId)
+            EchelonEvent? event_ = _storageService.GetEvent(eventId);
+
+            if (event_ is null)
+            {
+                return;
+            }
+
+            // Retrieve the Discord message
+            var channel = _client.GetChannel(event_.ChannelId) as IMessageChannel;
+            var message = await channel.GetMessageAsync(event_.MessageId) as IUserMessage;
+
+            if (message is null)
+            {
+                return;
+            }
+
+            IEnumerable<AttendeeRecord>? attendees = _storageService.GetAttendeeRecords(eventId);
+
+            Embed? embed;
+
+            if (cancelled)
+                embed = _embedFactory.CreateCancelledEventEmbed(event_);
+            else
+                embed = _embedFactory.CreateEventEmbed(event_, attendees);
+
+            // Modify the existing message with the updated embed
+            await message.ModifyAsync(msg =>
+            {
+                msg.Embed = embed;
+
+                if (cancelled || closed)
+                    msg.Components = new ComponentBuilder().Build();
+            });
+        }
+    }
+}
